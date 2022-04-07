9886e2ab9a6bd3b219118c0e79fc7f84d490db32
Tim Hsieh <tim@reslv.io>
2022-04-07 11:40:03 +0800

    allow Resolve office IP

--- cloud_armor_sec_policy.tf
+++ cloud_armor_sec_policy.tf
@@ -19,9 +19,9 @@ resource "google_compute_security_policy" "apigateway-sec-policy" {
     match {
       versioned_expr = "SRC_IPS_V1"
       config {
-        src_ip_ranges = ["114.37.147.144/32"]
+        src_ip_ranges = ["114.37.147.144/32","61.93.230.193/32"]
       }
     }
-    description = "Allow access from IPs in 114.37.147.144/32"
+    description = "Allow access from IPs in 114.37.147.144/32, 61.93.230.193/32"
   }
 }
