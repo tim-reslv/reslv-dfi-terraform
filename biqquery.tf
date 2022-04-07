78a5b8fdeff18f6ec14d99217c6e926d72eadb1b
Tim Hsieh <tim@reslv.io>
2022-04-07 15:26:28 +0800

    change location

--- biqquery.tf
+++ biqquery.tf
@@ -2,7 +2,7 @@ resource "google_bigquery_dataset" "dataset" {
   dataset_id                  = "example_dataset"
   friendly_name               = "test"
   description                 = "This is a test description"
-  location                    = "HK"
+  location                    = var.region
   default_table_expiration_ms = 3600000
 
   labels = {
