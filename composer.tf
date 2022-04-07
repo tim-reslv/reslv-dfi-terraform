51f605bc75ce2c31f5716b8397c22a14bb440379
Tim Hsieh <tim@reslv.io>
2022-04-07 09:31:14 +0800

    update composer

--- composer.tf
+++ composer.tf
@@ -5,42 +5,26 @@ resource "google_composer_environment" "dev" {
   config {
 
     node_config {
-      service_account = "example-account@example-project.iam.gserviceaccount.com"
-#      oauth_scopes = "[https://www.googleapis.com/auth/cloud-platform,https://www.googleapis.com/auth/bigquery]"
-#      node_count = 4
+      service_account = "1025339549236-compute@developer.gserviceaccount.com"
+      oauth_scopes = "[https://www.googleapis.com/auth/cloud-platform,https://www.googleapis.com/auth/bigquery]"
+      node_count = 3
       disk_size_gb = 100
       zone = var.zone
       machine_type = "n1-standard-2"
     }
-
     software_config {
+      image_version = "composer-1.18.5-airflow-2.2.3"
       python_version = "3"
-      image_version = "composer-1.18.4-airflow-1.10.15"
       scheduler_count = 1
     }
 
-#    database_config {
-#      machine_type = "db-n1-standard-2"
-#    }
-#    web_server_config {
-#      machine_type = "composer-n1-webserver-2"
-#    }
-#    web_server_network_access_control {
-#      allowed_ip_range {
-#        value = "192.0.2.0/24"
-#        description = "office net 1"
-#      }
-#      allowed_ip_range {
-#        value = "192.0.4.0/24"
-#        description = "office net 3"
-#      }
-#    }
+    database_config {
+      machine_type = "db-n1-standard-2"
+    }
 
-#    maintenance_window {
-#      start_time = "2021-01-01T01:00:00Z"
-#      end_time = "2021-01-01T07:00:00Z"
-#      recurrence = "FREQ=WEEKLY;BYDAY=SU,WE,SA"
-#    }
+    web_server_config {
+      machine_type = "composer-n1-webserver-2"
+    }
 
   }
 
@@ -58,7 +42,7 @@ resource "google_compute_network" "dev-composer-network" {
 
 resource "google_compute_subnetwork" "dev-composer-subnetwork" {
   name          = "dev-composer-subnetwork"
-  ip_cidr_range = "10.2.0.0/16"
+  ip_cidr_range = "10.170.0.0/20"
   region        = "asia-east2"
   network       = google_compute_network.dev-composer-network.id
 }
