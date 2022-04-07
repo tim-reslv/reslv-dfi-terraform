6aaf958e15e57860c6fd9e022844024e1c1efb4a
Tim Hsieh <tim@reslv.io>
2022-04-07 16:15:44 +0800

    update bigquery_table

--- biqquery.tf
+++ biqquery.tf
@@ -78,20 +78,17 @@ EOF
 
 }
 
-resource "google_bigquery_table" "sheet" {
-  dataset_id = google_bigquery_dataset.logging.dataset_id
-  table_id   = "sheet"
+resource "google_bigquery_table" "default" {
+  dataset_id = "${google_bigquery_dataset.logging.dataset_id}"
+  table_id   = "sales"
 
-  external_data_configuration {
-    autodetect    = true
-    source_format = "GOOGLE_SHEETS"
-
-    google_sheets_options {
-      skip_leading_rows = 1
-    }
-
-    source_uris = [
-      "https://docs.google.com/spreadsheets/d/123456789012345",
-    ]
+  time_partitioning {
+    type = "DAY"
   }
+
+  labels = {
+    env = "default"
+  }
+
+  schema = "${file("schema.json")}"
 }
