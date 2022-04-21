bb96e17c4938a71f20c3b7d9220cf10c63ad33f9
Tim Hsieh <tim@reslv.io>
2022-04-21 16:39:39 +0800

    using existing cloud run

--- cloudrun-lb.tf
+++ cloudrun-lb.tf
@@ -45,25 +45,25 @@ resource "google_compute_region_network_endpoint_group" "serverless_neg" {
   }
 }
 
-#resource "google_cloud_run_service" "dfi-cloud-run" {
-#  name     = "dfi-cloud-run"
-#  location = var.region
-#  project  = var.project_id
-#
-#  template {
-#    spec {
-#      containers {
-#        image = "gcr.io/cloudrun/hello"
-#      }
-#    }
-#  }
-#}
+resource "google_cloud_run_service" "dfi-cloud-run" {
+  name     = "dfi-cloud-run"
+  location = var.region
+  project  = var.project_id
 
-#resource "google_cloud_run_service_iam_member" "public-access" {
-#  location = google_cloud_run_service.dfi-cloud-run.location
-#  project  = google_cloud_run_service.dfi-cloud-run.project
-#  service  = google_cloud_run_service.dfi-cloud-run.name
-#  role     = "roles/run.invoker"
-#  member   = "allUsers"
-#}
+  template {
+    spec {
+      containers {
+        image = "gcr.io/dfi-yuu/dfi-cloud-run"
+      }
+    }
+  }
+}
+
+resource "google_cloud_run_service_iam_member" "public-access" {
+  location = google_cloud_run_service.dfi-cloud-run.location
+  project  = google_cloud_run_service.dfi-cloud-run.project
+  service  = google_cloud_run_service.dfi-cloud-run.name
+  role     = "roles/run.invoker"
+  member   = "allUsers"
+}
 # [END cloudloadbalancing_ext_http_cloudrun]
