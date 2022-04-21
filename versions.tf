a78c014b5fba44fd5880b2e0f4e42dca888df4ba
Tim Hsieh <tim@reslv.io>
2022-04-21 20:37:40 +0800

    update goolge provider version

--- versions.tf
+++ versions.tf
@@ -2,13 +2,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-#      version = ">= 3.53, < 5.0"
       version = ">= 4.18"
 
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-#      version = ">= 3.53, < 5.0"
       version = ">= 4.18"
     }
   }
